fbee548d35c7a289d4a267370ef7320810d37d1b
nndwn <nandawan.libya@gmail.com>
2024-03-10 13:13:13 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,45 +3,4 @@
     If you want to see my artwork, you can visit my <a href="https://www.behance.net/nndwn">Behance profile</a>. Besides that, I enjoy developing games, and you can find them on my <a href="https://play.google.com/store/apps/dev?id=5162259810918381426">Google Play</a> Store or <a href="https://github.com/nndwn">my Github</a>. 
     Oh, by the way, my social media handles are <a href="https://twitter.com/nndwn">X</a> and <a href="https://www.instagram.com/nndwn_/">Instagram </a>. Once again, thank you."</p>
 
-    <style>
-        @font-face {
-    font-family: unifont;
-    src: url(/unifont.woff2);
-    font-display: swap;
-  }
-  *,
-  ::after,
-  ::before {
-    box-sizing: border-box;
-  }
-  
-  body,
-  h1,
-  p {
-    margin: 0;
-  }
-  
-  body {
-    color: rgb(233, 233, 233);
-    font-family: unifont, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    background-color: rgb(2, 2, 2);
-  }
-  
-  a {
-    text-decoration: underline;
-    color: inherit;
-  }
-  
-  div#app {
-    display: flex;
-    height: 100vh;
-    align-items: center;
-    justify-content: center;
-  }
-  div#app > p {
-    width: 30rem;
-    padding-inline: 1rem;
-  }
-  
-    </style>
+    
