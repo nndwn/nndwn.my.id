2d29edad4b6a73ee169a4a0bb87941f98d11d5ee
nndwn <nandawan.libya@gmail.com>
2024-03-10 12:40:32 +0700

    update

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+
+<p>"Hello, I am Nandawan. Thank you for visiting my website. 
+  If you want to reach me, you can contact me via email at <a href="mailto:nandawan.libya@gmail.com">nandawan.libya@gmail.com</a>. 
+  If you want to see my artwork, you can visit my <a href="https://www.behance.net/nndwn">Behance profile</a>. Besides that, I enjoy developing games, and you can find them on my <a href="https://play.google.com/store/apps/dev?id=5162259810918381426">Google Play</a> Store or <a href="https://github.com/nndwn">my Github</a>. 
+  Oh, by the way, my social media handles are <a href="https://twitter.com/nndwn">X</a> and <a href="https://www.instagram.com/nndwn_/">Instagram </a>. Once again, thank you."</p>
